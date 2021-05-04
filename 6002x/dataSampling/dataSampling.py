6766c2a13397aa67f8681fa486c1571c7bab4b2f
Carlos Eduardo A.S <79329559+CarlosEduardoAS@users.noreply.github.com>
2021-05-04 16:21:39 -0300

    Update dataSampling.py

--- 6002x/dataSampling/dataSampling.py
+++ 6002x/dataSampling/dataSampling.py
@@ -51,30 +51,117 @@ def getMeanAndSDs(population, sample, verbose = False):
         
 
 random.seed(0)
-population = getHighs()
-sampleSize = 100
-numSamples = 2000
-maxMeanDiff = 0
-maxSDDiff = 0
-sampleMeans = []
-for i in range(numSamples):
-    sample = random.sample(population, sampleSize)
-    popMean, sampleMean, popSD, sampleSD =\
-        getMeanAndSDs(population, sample, verbose = False)
-    sampleMeans.append(sampleMean)
-    if abs(popMean - sampleMean) > maxMeanDiff:
-        maxMeanDiff = abs(popMean - sampleMean)
-    if abs(popSD - sampleSD) > maxSDDiff:
-        maxSDDiff = abs(popSD - sampleSD)
-print('Mean of sample Means =',
-      round(sum(sampleMeans)/len(sampleMeans), 3))
-print('Standard deviation of sample means =',
-      round(numpy.std(sampleMeans), 3))
-print('Maximum difference in means =',
-      round(maxMeanDiff, 3))
-print('Maximum difference in standard deviations =',
-      round(maxSDDiff, 3))
-makeHist(sampleMeans, 'Means of Samples', 'Mean', 'Frequency')
-pylab.axvline(x = popMean, color = 'r')
+# population = getHighs()
+# sampleSize = 200
+# numSamples = 1000
+# maxMeanDiff = 0
+# maxSDDiff = 0
+# sampleMeans = []
+# for i in range(numSamples):
+#     sample = random.sample(population, sampleSize)
+#     popMean, sampleMean, popSD, sampleSD =\
+#         getMeanAndSDs(population, sample, verbose = False)
+#     sampleMeans.append(sampleMean)
+#     if abs(popMean - sampleMean) > maxMeanDiff:
+#         maxMeanDiff = abs(popMean - sampleMean)
+#     if abs(popSD - sampleSD) > maxSDDiff:
+#         maxSDDiff = abs(popSD - sampleSD)
+# print('Mean of sample Means =',
+#       round(sum(sampleMeans)/len(sampleMeans), 3))
+# print('Standard deviation of sample means =',
+#       round(numpy.std(sampleMeans), 3))
+# print('Maximum difference in means =',
+#       round(maxMeanDiff, 3))
+# print('Maximum difference in standard deviations =',
+#       round(maxSDDiff, 3))
+# makeHist(sampleMeans, 'Means of Samples', 'Mean', 'Frequency')
+# pylab.axvline(x = popMean, color = 'r')
 
 
+#Standard Error
+def sem(popSD, sampleSize):
+    return popSD/sampleSize**0.5
+
+
+# sampleSizes = (25, 50, 100, 200, 300, 400, 500, 600)
+# numTrials = 50
+# population = getHighs()
+# popSD = numpy.std(population)
+# sems = []
+# sampleSDs = []
+# for size in sampleSizes:
+#     sems.append(sem(popSD, size))
+#     means = []
+#     for t in range(numTrials):
+#         sample = random.sample(population, size)
+#         means.append(sum(sample)/len(sample))
+#     sampleSDs.append(numpy.std(means))
+# pylab.plot(sampleSizes, sampleSDs,
+#            label = 'Std of 50 means')
+# pylab.plot(sampleSizes, sems, 'r--', label = 'SEM')
+# pylab.title('SEM vs. SD for 50 Means')
+# pylab.legend()
+
+
+def getDiffs(population, sampleSizes):
+    popStd = numpy.std(population)
+    diffsFracs = []
+    for sampleSize in sampleSizes:
+        diffs = []
+        for t in range(100):
+            sample = random.sample(population, sampleSize)
+            diffs.append(abs(popStd - numpy.std(sample)))
+        diffMean = sum(diffs)/len(diffs)
+        diffsFracs.append(diffMean/popStd)
+    return pylab.array(diffsFracs)*100
+    
+def plotDiffs(sampleSizes, diffs, title, label):
+    pylab.plot(sampleSizes, diffs, label = label)
+    pylab.xlabel('Sample Size')
+    pylab.ylabel('% Difference in SD')
+    pylab.title(title)
+    pylab.legend()
+
+# sampleSizes = range(20, 600, 1)
+# diffs = getDiffs(getHighs(), sampleSizes)
+# plotDiffs(sampleSizes, diffs,
+#           'Sample SD vs Population SD, Temperatures',
+#            label = 'High temps')
+
+
+def plotDistributions():
+    uniform, normal, exp = [], [], []
+    for i in range(100000):
+        uniform.append(random.random())
+        normal.append(random.gauss(0, 1))
+        exp.append(random.expovariate(0.5))
+    makeHist(uniform, 'Uniform', 'Value', 'Frequency')
+    pylab.figure()
+    makeHist(normal, 'Gaussian', 'Value', 'Frequency')
+    pylab.figure()
+    makeHist(exp, 'Exponencial', 'Value', 'Frequency')
+    
+    
+#plotDistributions()
+
+def compareDists():
+    uniform, normal, exp = [], [], []
+    for i in range(100000):
+        uniform.append(random.random())
+        normal.append(random.gauss(0, 1))
+        exp.append(random.expovariate(0.5))
+    sampleSizes = range(20, 600, 1)
+    udiffs = getDiffs(uniform, sampleSizes)
+    ndiffs = getDiffs(normal, sampleSizes)
+    ediffs = getDiffs(exp, sampleSizes)
+    plotDiffs(sampleSizes, udiffs,
+              'Sample SD vc Population SD',
+              'Uniform population')
+    plotDiffs(sampleSizes, ndiffs,
+              'Sample SD vs Population SD',
+              'Normal population')
+    plotDiffs(sampleSizes, ediffs,
+              'Sample SD vs Population SD',
+              'Exponencial population')
+
+compareDists()
